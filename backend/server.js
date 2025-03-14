7014b5913cc87912a91529c559b84c1b514c8e05
Moumita Ray <moumitaray@Moumitas-MacBook-Air.local>
2025-03-14 17:38:10 -0400

    Added a backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import "dotenv/config";
+const requestHandler = express();
+const port = process.env.PORT;
+requestHandler.use(express.json());
+import "dotenv/config"
+PORT = 3000
+requestHandler.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
