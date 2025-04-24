89cd4a3018ae40beb6f56e61801ee8486d457bd0
Moumita Ray <moumitaray@Moumitas-MacBook-Air.local>
2025-04-23 20:57:04 -0400

    edited some functions for App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,3 +3,30 @@ import DropdownMenu from "./DropdownMenu";
 import BarChart from "./BarChart";
 import PieChart from "./PieChart";
 import AboutMe from "./AboutMe";
+
+function App() {
+  const [selection, setSelection] = useState("");
+
+  return (
+    <div className="App">
+      <h1>Education Dashboard</h1>
+      <DropdownMenu onSelect={setSelection} />
+      <div id="output">
+        {selection === "totalELL" && (
+          <p>Total ELLs in 50 states: 4,754,139</p>
+        )}
+        {selection === "studentsWithDisabilities" && (
+          <>
+            <p>Total students with disabilities in Michigan: 660,357</p>
+            <p>Percentage: 13.9%</p>
+          </>
+        )}
+        {selection === "bar" && <BarChart />}
+        {selection === "pie" && <PieChart />}
+        {selection === "about" && <AboutMe />}
+      </div>
+    </div>
+  );
+}
+
+export default App;
