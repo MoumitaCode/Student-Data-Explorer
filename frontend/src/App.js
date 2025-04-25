bd3725f9547bd973ab34d05793ed869a349436de
Moumita Ray <moumitaray@Moumitas-MacBook-Air.local>
2025-04-25 16:40:27 -0400

    Edited the title of some dropdown menu and the title of website

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Education Dashboard</h1>
+      <h1>EquiLang(dummy data)</h1>
       <DropdownMenu onSelect={setSelection} />
       <div id="output">
         {selection === "totalELL" && (
--- frontend/src/DropdownMenu.js
+++ frontend/src/DropdownMenu.js
@@ -4,8 +4,8 @@ const DropdownMenu = ({ onSelect }) => {
   return (
     <select onChange={(e) => onSelect(e.target.value)}>
       <option value="">Select Option</option>
-      <option value="totalELL">Total ELL Students</option>
-      <option value="studentsWithDisabilities">Students with Disabilities (MI)</option>
+      <option value="totalELL">Display the total ELL's in 50 states</option>
+      <option value="studentsWithDisabilities">Students with Disabilities in Michigan</option>
       <option value="bar">Show Bar Chart</option>
       <option value="pie">Show Pie Chart</option>
       <option value="about">About Me</option>
